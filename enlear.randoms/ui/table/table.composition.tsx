d5a35382c4e364a87d83e8c499955ba425c156c6
ChameeraD <chameeradulanga87@gmail.com>
2021-04-02 18:17:19 +0530

    table component added

--- enlear.randoms/ui/table/table.composition.tsx
+++ enlear.randoms/ui/table/table.composition.tsx
@@ -3,5 +3,5 @@ import { Table } from './table';
 
 // sets the Component preview in gallery view
 export const BasicTable = () => {
-  return <Table>click me</Table>;
+  return <Table ></Table>;
 };
--- enlear.randoms/ui/table/table.tsx
+++ enlear.randoms/ui/table/table.tsx
@@ -1,10 +1,116 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useTable, useFilters, useSortBy } from "react-table";
+import "./styles.css";
+import makeData from './makeData';
+import styled from 'styled-components';
+
 
 export interface TableProps extends React.HTMLAttributes<HTMLButtonElement> {
 
 };
 
-export const Table = ({ children, ...rest }: TableProps) => {
-  return <button {...rest}>{children}</button>
-};
+
+function Main({ columns, data }) {
+  const [filterInput, setFilterInput] = useState("");
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    setFilter
+  } = useTable({
+    columns, data
+  },
+  useFilters,
+  useSortBy
+  )
+  const handleFilterChange = e => {
+    const value = e.target.value || undefined;
+    setFilter("firstName", value);
+    setFilterInput(value);
+  };
+  // Render the UI for your table
+  return (
+    <div>
+      <input
+        value={filterInput}
+        onChange={handleFilterChange}
+        placeholder={"Search with First Name"}
+      />
+    <table {...getTableProps()}>
+      <thead>
+        {headerGroups.map(headerGroup => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map(column => (
+              <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row, i) => {
+          prepareRow(row)
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map(cell => {
+                return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+    </div>
+  )
+}
+
+
+export const Table = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(20), []);
+  return (
+      <Main columns={columns} data={data} />
+  );
+}
+
